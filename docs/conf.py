bfea76db70002c40c361f7ca3edb3c8cb0c972a4
RavinduNF <fernandoravindu1994@gmail.com>
2019-06-03 00:07:36 +0530

    Adding docs folder and license content update

---
+++ docs/conf.py
@@ -0,0 +1,6 @@
+commands = {
+    'git_branch' : "git branch | grep \*" + " | cut -d ' ' -f2",
+    'git_pull' : "git pull",
+    'git_reset_hard': "git reset --hard origin/",
+    'git_checkout_master': "git checkout master"
+}
